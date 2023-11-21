962a621803f71695f24143e5736b23ca5e43c4d6
Stiven Gomez <stiven.gomez@nuvu.cc>
2023-11-20 19:19:57 -0500

    Imagen escritorio.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,12 @@ function App() {
 
   return (
     <>
-      <div>
+
+      <div className='home_desk'>
+      </div>
+
+
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +32,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
