244d95f01a938c59256dbd40885da4c1288fd075
Stiven Gomez <stiven.gomez@nuvu.cc>
2023-11-22 12:30:21 -0500

    Ajuste de className.

--- src/components/widgets/item_program/item_program.jsx
+++ src/components/widgets/item_program/item_program.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import './item_program.css';
 import GoogleChromeIcon from '../../../assets/icons/google-chrome-icon.png';
 
 export function ItemProgram({ infoProgram }) {
 
+    const [className, setClassName] = useState('');
+
+    function clickSelect() {
+        setClassName('focusProgram')
+        console.log("Se da click al programa");
+    }
+
+    function openProgram() {
+        console.log("Se abre el programa");
+    }
+
     return (
-        <div className="item_program">
+        <div className={'item_program ' + className} onDoubleClick={openProgram} onClick={clickSelect}>
             <img src={'/src/assets/icons/' + infoProgram.icon1 + '.png'} alt="" className='icon_program' />
             <span className='name_program'>{infoProgram.name}</span>
         </div>
